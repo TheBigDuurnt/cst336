44d75ef17f5f3f50e5932cdf44f178eced77f823
Austin Walker <auwalker@csumb.edu>
2018-07-16 16:59:52 +0000

    heroku update + some lab6

---
+++ labs/lab6/login.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Ottermart - Admin Site</title>
+    </head>
+    <body>
+        <h1> Ottermart Admin Site</h1>
+        
+        
+        <form method="POST" action="loginProcess.php">
+            Username: <input type="text" name="username" /> <br />
+            Password: <input type="password" name="password" /> <br />
+            
+            <input type="submit" name="submitForm" value="Login" />
+            <?php
+            
+                if($_SESSION['incorrect']) {
+                    echo "<p class = 'lead' id='error' stype='color:red'>";
+                    echo "<strong>Incorrect Username or Password!</strong></p>";
+                }
+            
+            
+            ?>
+        </form>
+    </body>
+</html>
